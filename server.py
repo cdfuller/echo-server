4d91fd686c22fc0924f1a6fe043b4808c3ef2ab2
Cody Fuller <git@codyfuller.com>
2017-12-27 18:19:53 -0600

    Add argument options

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import socket
 import datetime
 
-HOST, PORT = '0.0.0.0', 3000
 
-def run():
+def run(host='0.0.0.0', port=3000):
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listen_socket.bind((HOST, PORT))
+    listen_socket.bind((host, port))
     listen_socket.listen(1)
-    print(f'Serving at http://{HOST}:{PORT}')
+    print(f'Serving at http://{host}:{port}')
     while True:
         client_connection, client_address = listen_socket.accept()
         request = client_connection.recv(1024).decode()
@@ -25,4 +24,12 @@ HTTP/1.1 200 OK
         client_connection.close()
 
 if __name__ == '__main__':
-    run()
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=3000, type=int, help='port to listen on')
+    parser.add_argument('--host', default='localhost', help='host to bind to')
+    args = parser.parse_args()
+    port = args.port
+    host = args.host
+    run(host=host, port=port)
